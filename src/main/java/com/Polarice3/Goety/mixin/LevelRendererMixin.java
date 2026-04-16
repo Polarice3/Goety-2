bac066c89026d4feb1d0cee48c3e86efc7c1e798
mikasa <1244456115@qq.com>
2026-04-16 13:51:09 +0800

    Use mixinextras to avoid some mixin conflicts, and revert `goetySetTeamColor` in LevelRendererMixin

--- src/main/java/com/Polarice3/Goety/mixin/LevelRendererMixin.java
+++ src/main/java/com/Polarice3/Goety/mixin/LevelRendererMixin.java
@@ -1,12 +1,16 @@
 package com.Polarice3.Goety.mixin;
 
+import com.Polarice3.Goety.client.events.ClientEvents;
 import com.Polarice3.Goety.client.particles.LightningEffect;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.RenderBuffers;
+import net.minecraft.world.entity.Entity;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -33,5 +37,18 @@ public class LevelRendererMixin {
     private void renderOverlays(PoseStack ps, float partialTicks, long unknown, boolean drawBlockOutline,
                                 Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f projMat, CallbackInfo ci) {
         LightningEffect.onWorldRenderLast(camera, partialTicks, ps, renderBuffers);
+    }
+
+    @WrapOperation(
+            method = "renderLevel(Lcom/mojang/blaze3d/vertex/PoseStack;FJZLnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/GameRenderer;Lnet/minecraft/client/renderer/LightTexture;Lorg/joml/Matrix4f;)V",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/Entity;getTeamColor()I"
+            )
+    )
+    private int goetySetTeamColor(Entity entity, Operation<Integer> original) {
+        if (ClientEvents.target == entity) {
+            return 0xffd83e;
+        }
+        return original.call(entity);
     }
 }
