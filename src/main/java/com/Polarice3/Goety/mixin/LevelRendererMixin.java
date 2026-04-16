0d0e543845b6d07a88c03693bd4ca5c7c63178d3
Polarice3 <zarularief04@gmail.com>
2026-04-16 12:31:33 +0800

    Fixed compat issues with Citadel

--- src/main/java/com/Polarice3/Goety/mixin/LevelRendererMixin.java
+++ src/main/java/com/Polarice3/Goety/mixin/LevelRendererMixin.java
@@ -1,6 +1,5 @@
 package com.Polarice3.Goety.mixin;
 
-import com.Polarice3.Goety.client.events.ClientEvents;
 import com.Polarice3.Goety.client.particles.LightningEffect;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Camera;
@@ -8,14 +7,12 @@ import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.RenderBuffers;
-import net.minecraft.world.entity.Entity;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LevelRenderer.class)
@@ -36,19 +33,5 @@ public class LevelRendererMixin {
     private void renderOverlays(PoseStack ps, float partialTicks, long unknown, boolean drawBlockOutline,
                                 Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f projMat, CallbackInfo ci) {
         LightningEffect.onWorldRenderLast(camera, partialTicks, ps, renderBuffers);
-    }
-
-    @Redirect(
-            method = "renderLevel(Lcom/mojang/blaze3d/vertex/PoseStack;FJZLnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/GameRenderer;Lnet/minecraft/client/renderer/LightTexture;Lorg/joml/Matrix4f;)V",
-            at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/world/entity/Entity;getTeamColor()I"
-            )
-    )
-    private int goetySetTeamColor(Entity entity) {
-        int color = entity.getTeamColor();
-        if (ClientEvents.target == entity) {
-            color = 0xffd83e;
-        }
-        return color;
     }
 }
